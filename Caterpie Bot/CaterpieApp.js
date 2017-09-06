68d48b4394634ac8c4ea5def963b82ab94338350
tad947 <tad947@yahoo.com>
2017-09-05 22:35:59 -0400

    Added CaterpieBot
    Added the coding for CaterpieBot to repository.

---
+++ Caterpie Bot/CaterpieApp.js
@@ -0,0 +1,108 @@
+//Call package
+var Discord = require("discord.js");
+var caterpieBot = new Discord.Client();
+var nextClubMeeting;
+
+//Listener Event for recieved messages
+caterpieBot.on("message", message =>
+{
+  var commands = [" !Terminate", " !Restart", " !Commands", " !Caterpiefact", " !Pokefact"];
+  var pokeFacts = ["Cubone wears the skull of its dead mother like a mask.",
+  "The name Pokemon isn't Japanese at all! It's really just a combination of 'pocket' and 'monster.'",
+  "Kabutops cut open prey with their claws, then suck up the spilled bodily fluids."];
+  var randomNumber;
+
+  var sender = message.author; // who sent the message
+  var msg = message.content.toUpperCase(); // convert message to all uppercase letters
+  var prefix = "!" //command to interact with caterpieBot
+  var tempMsg = msg;
+  var splitMsg = tempMsg.split(":")
+
+
+  if(splitMsg[0] === "CATERPIE, PLEASE SCHEDULE OUR NEXT CLUB MEETING FOR")
+  {
+      message.channel.send("Yes " + sender + ", I will schedule the next club meeting for: " + splitMsg[1])
+
+      nextClubMeeting = splitMsg[1];
+      //File fileee = new File("C:\Users\bbbbbbbbbbbbbbbbbb\Desktop\Caterpie Bot");
+      //BufferedWriter writer = new BufferedWriter(new FileWriter(new File("ClubMeetingInfo.txt")));
+    //  writer.write(nextClubMeeting);
+      //writer.close();
+
+      console.log(nextClubMeeting);
+  }
+
+  if(msg === prefix + "CLUBMEETING")
+  {
+      //message.channel.send(msg) // reply to "!caterpiefact"
+      message.channel.send("The next club meeting is scheduled for: " + nextClubMeeting)
+  }
+
+  if(msg === prefix + "CATERPIE")
+  {
+      //message.channel.send(msg) // reply to "!caterpiefact"
+      message.channel.send("test!!")
+
+  }
+
+  if(msg === prefix + "TERMINATE")
+  {
+      throw new Error("Process ended.");
+  }
+
+  if(msg === prefix + "RESTART")
+  {
+      Process.RESTART();
+  }
+
+  if(msg === prefix + "COMMANDS") //command that a user may enter
+  {
+      message.channel.send("Here is the current list of Commands: " + commands) // reply to "!caterpiefact"
+  }
+
+//  if(msg === prefix + "CATERPIEFACT") //command that a user may enter
+//  {
+//      message.channel.send("There are currently no facts in this database.") // reply to "!caterpiefact"
+//  }
+
+  if(msg === prefix + "POKEFACT") //command that a user may enter
+  {
+      randomNumber = Math.floor(Math.random() * pokeFacts.length);
+      message.channel.send(pokeFacts[randomNumber]) // reply to "!caterpiefact"
+  }
+});
+
+caterpieBot.on("ready", () =>
+{
+  Clock();
+});
+
+caterpieBot.login("MzUzOTY0NjE4OTQ4ODA0NjA4.DI3W5Q.z5A6nlMHyVJCE8e-ZfrkQ8kGP_I") //Bot"s token found in https://discordapp.com/developers/applications/me/353964618948804608?success=created
+
+function Clock ()
+{
+    var currentTime = new Date ( );
+
+    var currentHours = currentTime.getHours ( );
+    var currentMinutes = currentTime.getMinutes ( );
+    var currentSeconds = currentTime.getSeconds ( );
+
+    // Pad the minutes and seconds with leading zeros, if required
+    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+    // Choose either "AM" or "PM" as appropriate
+    var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+
+    // Convert the hours component to 12-hour format if needed
+    currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+
+    // Convert an hours component of "0" to "12"
+    currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+
+    // Compose the string for display
+    currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+    //Send the time to channel along with a welcome message
+    caterpieBot.channels.find("name", "bot-testing").send("Caterpie Bot is now online. The time is: " + currentTimeString);
+}
